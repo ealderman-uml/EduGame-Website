05c3c9f3433b6120e4478010f4dc60aa97d1e624
Ed Alderman <edward_alderman@student.uml.edu>
2024-11-12 05:11:28 +0000

    Create basic site skeleton
    Added Header, Footer, Sidebar, and ContentFrame components to be rendered on all pages.
    Created placeholder pages for ProjectBackground, ProjectDescription, and TeamInfo.
    Adjusted project defaults.
    
    Signed-off-by: Ed Alderman <edward_alderman@student.uml.edu>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
